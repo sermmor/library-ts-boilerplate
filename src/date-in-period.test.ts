5784ab71c05ddb519b13bb81905cb40312a0021a
Sergio Martín <sermmor@outlook.com>
2019-04-27 13:59:45 +0200

    Added all tests.

--- src/date-in-period.test.ts
+++ src/date-in-period.test.ts
@@ -1,20 +1,189 @@
-import { validateDateInPeriod } from './date-in-period';
+import { validateDateInPeriod, VALIDATION_TYPE } from './date-in-period';
+import { FieldValidationResult } from 'lc-form-validation';
 
-describe('myFunction', () => {
-  it('should return true', () => {
-    // Arrage & Act & Assert
-    // console.log(`WOLOLOOO... ${splitDate("1986/october/09")}`);
-    // console.log(`WOLOLOOO... ${splitDate("1986-october-09")}`);
-    // console.log(`WOLOLOOO... ${splitDate("1986-FeBRuary-28")}`);
+const validMessage: string = '';
 
-    // console.log(`WOLOLOOO... ${isDateInPeriod("1986/10/09", "1986-FeBRuary-28", "1989-12-31")}`); // TRUE
-    // console.log(`WOLOLOOO... ${isDateInPeriod("2019/5/21", "2019-5-28", "2019-6-17")}`); // FALSE
-    // console.log(`WOLOLOOO... ${isDateInPeriod("2019/5/30", "2019-5-28", "2019-6-17")}`); // TRUE
-    // console.log(`WOLOLOOO... ${isDateInPeriod("2018/12/31", "2018-12-25", "2019-01-6")}`); // TRUE
-    // console.log(`WOLOLOOO... ${isDateInPeriod("2018-12-29", "2018-11-30", "2019-2-14")}`); // TRUE
-    // console.log(`WOLOLOOO... ${isDateInPeriod("2019-2-12", "2018-11-30", "2019-2-14")}`); // TRUE
-    // console.log(`WOLOLOOO... ${isDateInPeriod("2019-2-16", "2018-11-30", "2019-2-14")}`); // FALSE
+const expectValidationResultToBeFalsy = (result: FieldValidationResult) => {
+  expect(result.type).toEqual(VALIDATION_TYPE);
+  expect(result.succeeded).toBeFalsy();
+  expect(result.errorMessage === validMessage).toBeFalsy();
+}
 
-    //expect(myFunction(undefined)).toBeTruthy();
+const expectValidationResultToBeTruthy = (result: FieldValidationResult) => {
+  expect(result.type).toEqual(VALIDATION_TYPE);
+  expect(result.succeeded).toBeTruthy();
+  expect(result.errorMessage).toEqual(validMessage);
+}
+
+describe('validateDateInPeriod', () => {
+  it('Check undefined date in period', () => {
+    // Arrange
+    const inputDate: string = undefined;
+    const period: string[] = ["1994/august/06", "1994/august/06"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeFalsy(result);
+
   });
+
+  it('Check null date in period', () => {
+    // Arrange
+    const inputDate: string = null;
+    const period: string[] = ["1994/august/06", "1994/august/06"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeFalsy(result);
+
+  });
+
+  it('Check date in period with same begin and ending', () => {
+    // Arrange
+    const inputDate: string = "1994/august/06";
+    const period: string[] = ["1994/august/06", "1994/august/06"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeFalsy(result);
+  });
+
+  it('Check date in period with bars', () => {
+    // Arrange
+    const inputDate: string = "1994-august-07";
+    const period: string[] = ["1994/august/06", "1994/august/08"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
+  it('Check date with case in month', () => {
+    // Arrange
+    const inputDate: string = "1994-FeBRuary-07";
+    const period: string[] = ["1994/FeBRuary/06", "1994/FeBRuary/08"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
+  it('Check date with numbers in month', () => {
+    // Arrange
+    const inputDate: string = "1994-07-07";
+    const period: string[] = ["1994/07/06", "1994-07-8"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
+  it('Check date with numbers in month and string in ending period', () => {
+    // Arrange
+    const inputDate: string = "1994-7-07";
+    const period: string[] = ["1994-7-06", "1994-july-8"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
+  it('Check date in period with beginning greater than ending', () => {
+    // Arrange
+    const inputDate: string = "1994-10-27";
+    const period: string[] = ["1994-FeBRuary-28", "1989-12-31"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeFalsy(result);
+  });
+
+  it('Check date less than begin period', () => {
+    // Arrange
+    const inputDate: string = "2019/5/21";
+    const period: string[] = ["2019-5-28", "2019-6-17"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeFalsy(result);
+  });
+
+  it('Check date greater than end period', () => {
+    // Arrange
+    const inputDate: string = "2019/6/30";
+    const period: string[] = ["2019-5-28", "2019-6-17"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeFalsy(result);
+  });
+  
+  it('Check date in period with same year but diferent month', () => {
+    // Arrange
+    const inputDate: string = "1994-10-27";
+    const period: string[] = ["1994-FeBRuary-28", "1994-12-31"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
+  it('Check date in period with same year and same beginning month', () => {
+    // Arrange
+    const inputDate: string = "2019/5/30";
+    const period: string[] = ["2019-5-28", "2019-6-17"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+  
+  it('Check date in period with same year and same ending month', () => {
+    // Arrange
+    const inputDate: string = "2019-2-12";
+    const period: string[] = ["2018-11-30", "2019-2-14"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
+  it('Check date in period with different year', () => {
+    // Arrange
+    const inputDate: string = "2018-12-29";
+    const period: string[] = ["2017-11-30", "2019-2-14"];
+
+    // Act.
+    const result: FieldValidationResult = validateDateInPeriod(inputDate, period[0], period[1]);
+
+    // Assert.
+    expectValidationResultToBeTruthy(result);
+  });
+
 });
