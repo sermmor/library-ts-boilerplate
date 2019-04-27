8d47607960fbaadacf230cc00fc01168d0280cdc
Sergio Martín <sermmor@outlook.com>
2019-04-27 12:48:50 +0200

    Added url to readme.

--- index.d.ts
+++ index.d.ts
@@ -5,5 +5,6 @@
  * @param dateEnding date (with format Year-Month-Day or Year/Month/Day) ending of the period.
  */
 import FieldValidationResult from 'lc-form-validation';
+
 export declare const VALIDATION_TYPE : string;
 export declare function validateDateInPeriod(dateInput: string, dateBeginning: string, dateEnding: string): FieldValidationResult;
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 # library-validator-date-in-period
-Microlibrary for validate 1 date between 2 dates (using lc-form-validation library).
+Library for validate 1 date between 2 dates (using [lc-form-validation](https://github.com/Lemoncode/lcFormValidation) library).
